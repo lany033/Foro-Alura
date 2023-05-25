1265c01d1b3d3c3e99e5a7bc5ef8173c46b13825
Melanie <mmantilla930@gmail.com>
2023-05-24 19:10:51 -0500

    tratamiento de errores

---
+++ src/main/java/com/alura/foro/ForoAlura/infra/TratadorDeErrores.java
@@ -0,0 +1,38 @@
+package com.alura.foro.ForoAlura.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity tratarError406(SQLIntegrityConstraintViolationException e){
+        var error = e.getMessage();
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    private record DatosErrorValidacion(String campo, String error){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
