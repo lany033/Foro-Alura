cd4fc51c9565a742597a133c2663743a4c2b9724
Melanie <mmantilla930@gmail.com>
2023-05-26 20:06:16 -0500

    tabla usuarios

---
+++ src/main/java/com/alura/foro/ForoAlura/controller/AutenticacionController.java
@@ -0,0 +1,32 @@
+package com.alura.foro.ForoAlura.controller;
+
+import com.alura.foro.ForoAlura.domain.usuarios.DatosAutenticacionUsuario;
+import com.alura.foro.ForoAlura.domain.usuarios.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/login")
+public class AutenticacionController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
+
+    //DTO -> Necesario cuando se recibe parametros desde el cliente
+    @PostMapping
+    public ResponseEntity autenticacionUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
+                datosAutenticacionUsuario.clave());
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
+    }
+}
---
+++ src/main/java/com/alura/foro/ForoAlura/domain/usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package com.alura.foro.ForoAlura.domain.usuarios;
+
+public record DatosAutenticacionUsuario(String login, String clave) {
+}
